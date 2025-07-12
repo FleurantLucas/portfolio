d3553e307541074070272116c552646f7dc89c50
LUCAS-PC\luluP <lulufleurant@hotmail.com>
2025-07-12 14:16:53 -0400

    Ajout du reste des sections du one-pager

--- src/app/competence/competence.component.ts
+++ src/app/competence/competence.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-competence',
   templateUrl: './competence.component.html',
-  styleUrls: ['./competence.component.css']
+  styleUrls: ['./competence.component.css'],
+  imports: [CommonModule],
 })
 export class CompetenceComponent implements OnInit {
+
+  skills = [
+  { name: 'Angular', level: 8 },
+  { name: 'TypeScript', level: 8 },
+  { name: 'HTML', level: 9 },
+  { name: 'CSS', level: 6 },
+  { name: 'PHP', level: 7 },
+  { name: 'JavaScript', level: 6 },
+  { name: 'SQL', level: 8 },
+  { name: 'C#', level: 10 },
+  { name: 'ASP.NET', level: 10 },
+  { name: 'Android Studio', level: 9 },
+  { name: 'Flutter', level: 9 },
+  { name: 'Java', level: 8 },
+];
 
   constructor() { }
 
